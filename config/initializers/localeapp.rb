747ac732c34a90bda41276fd0a9e6ac8aff8cc69
Charles Bacon <charles@247Seas.com>
2012-12-17 21:07:37 +0000

    Update bug fixes and updates to staging

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -2,4 +2,5 @@ require 'localeapp/rails'
 
 Localeapp.configure do |config|
   config.api_key = 'hS5oqivMKNmqzRbsIWurtkVn2He8QXmNqySebcfHk7dokM8IJg'
+  config.polling_environments = [:staging] # Doesn't include :production
 end
