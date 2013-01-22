a029d769fce0718308804f6717472f307c8b6113
Charles Bacon <charles@247Seas.com>
2013-01-22 22:08:35 +0000

    added James's styling

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -2,5 +2,7 @@ require 'localeapp/rails'
 
 Localeapp.configure do |config|
   config.api_key = 'hS5oqivMKNmqzRbsIWurtkVn2He8QXmNqySebcfHk7dokM8IJg'
-  config.polling_environments = [:staging] # Doesn't include :production
+  config.polling_environments = [:development] # Doesn't include :staging or :production
+  #config.sending_environments = [:development]
+  #config.polling_environments = [:staging] # Doesn't include :production
 end
