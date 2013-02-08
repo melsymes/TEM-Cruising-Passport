ecb96ea56755bd7fa5c5d1aae09ebdc0d348c384
Charles Bacon <charles@247Seas.com>
2013-02-08 21:13:14 +0000

    added localeapp to staging

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -2,7 +2,19 @@ require 'localeapp/rails'
 
 Localeapp.configure do |config|
   config.api_key = 'hS5oqivMKNmqzRbsIWurtkVn2He8QXmNqySebcfHk7dokM8IJg'
-  config.polling_environments = [:development] # Doesn't include :staging or :production
+  #config.polling_environments = [:development] # Doesn't include :staging or :production
   #config.sending_environments = [:development]
   #config.polling_environments = [:staging] # Doesn't include :production
+
+  config.poll_interval = 300 if Rails.env.staging?
+  config.polling_environments = [:development, :staging]
+  config.reloading_environments = [:development, :staging]
+  config.sending_environments = [:development, :staging]
 end
+
+# Pull latest when dyno restarts on staging
+if Rails.env.staging?
+  Localeapp::CLI::Pull.new.execute
+end
+
+
