a74d125235236b71ffe49515bbb3fb63d179ba79
Charles Bacon <charles@247Seas.com>
2013-02-11 22:59:36 +0000

    added localapp to heroku

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,12 +1,15 @@
 require 'localeapp/rails'
 
 Localeapp.configure do |config|
-  config.api_key = 'hS5oqivMKNmqzRbsIWurtkVn2He8QXmNqySebcfHk7dokM8IJg'
+  #config.api_key = 'hS5oqivMKNmqzRbsIWurtkVn2He8QXmNqySebcfHk7dokM8IJg'
   #config.polling_environments = [:development] # Doesn't include :staging or :production
   #config.sending_environments = [:development]
   #config.polling_environments = [:staging] # Doesn't include :production
-
-  config.poll_interval = 300 if Rails.env.staging?
+  config.api_key = '9v1SZrDwelpr9gLmX4ScPySkNE2bQjXtmhY4s6zP5nNJ1Kq1gn'
+#  config.poll_interval = 300 if Rails.env.staging?
+#
+#
+  config.environment_name = ENV['LOCALEAPP_ENV'] unless ENV['LOCALEAPP_ENV'].nil?
   config.polling_environments = [:development, :staging]
   config.reloading_environments = [:development, :staging]
   config.sending_environments = [:development, :staging]
