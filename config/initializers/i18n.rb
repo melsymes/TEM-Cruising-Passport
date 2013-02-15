2513e0bb6b1da1bf9aea8eee0dc2d3ff321b1114
Charles Bacon <charles@247Seas.com>
2013-02-15 19:20:03 +0000

    added French translation files and sorted locale button styling

---
+++ config/initializers/i18n.rb
@@ -0,0 +1,9 @@
+#encoding: utf-8
+#See http://www.ascii.cl/htmlcodes.htm for help - use HTML name
+
+I18n.default_locale = :en
+LANGUAGES = [
+['English', 'en'],
+["Espa&ntilde;ol".html_safe, 'es-ES'],
+["Fran&ccedil;ais".html_safe, 'fr-FR' ]
+]
