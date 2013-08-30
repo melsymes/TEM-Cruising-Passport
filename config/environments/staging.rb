25b16787adb3ef3dd97d30ff9d6408dd61a404ec
Charles Bacon <charles@247Seas.com>
2013-08-30 06:50:23 +0100

    fixes to host - for password rest

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -14,7 +14,7 @@ Myapp10::Application.configure do
   config.action_controller.perform_caching = false
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'http://myapp10-staging.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'http://staging.cruisingpassport.com/' }
   config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
   config.action_mailer.perform_deliveries = false
@@ -34,7 +34,7 @@ Myapp10::Application.configure do
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 587,
-    :domain    => "myapp10-staging.herokuapp.com",
+    :domain    => "staging.cruisingpassport.com/",
     :user_name => 'thewaypoint',
     :password  => '4526889d-079d-41ee-ba4b-c4331cd0e409'
   }
