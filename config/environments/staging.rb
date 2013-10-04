e5211d04405c8be628782ed629b295deefa85f2d
Charles Bacon <charles@247Seas.com>
2013-10-04 09:20:42 +0100

    changing staging env to send emails

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -17,7 +17,7 @@ Myapp10::Application.configure do
   config.action_mailer.default_url_options = { :host => 'http://staging.cruisingpassport.com/' }
   config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 
